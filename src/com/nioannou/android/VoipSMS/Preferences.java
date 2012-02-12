3bab1a25ff32e7fa291d3b552a32f02412a2ce36
Nikolas Ioannou <nikolas@nikolas-desktop.(none)>
2012-02-12 11:03:03 +0000

    Initial commit of VoipSMS

---
+++ src/com/nioannou/android/VoipSMS/Preferences.java
@@ -0,0 +1,19 @@
+package com.nioannou.android.VoipSMS;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Preferences extends PreferenceActivity {
+
+	
+/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    addPreferencesFromResource(R.xml.preferences);
+	}
+	
+	
+
+}
---
+++ src/com/nioannou/android/VoipSMS/StringUtility.java
@@ -0,0 +1,17 @@
+package com.nioannou.android.VoipSMS;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringUtility {
+	public static List<String> splitEqually(String text, int size) {
+	    // Give the list the right capacity to start with. You could use an array
+	    // instead if you wanted.
+	    List<String> ret = new ArrayList<String>((text.length() + size - 1) / size);
+
+	    for (int start = 0; start < text.length(); start += size) {
+	        ret.add(text.substring(start, Math.min(text.length(), start + size)));
+	    }
+	    return ret;
+	}
+}
